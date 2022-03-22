961fb5295f51f9153d4c6991fe3f886142e1b052
affigueir <affigueir@gmail.com>
2022-03-22 14:54:50 +0000

    xml comments added

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,20 +4,38 @@ namespace MyFirstMethod
 {
     class Program
     {
+        /// <summary>
+        /// Method testing main 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Tell me a number:");
             int aux = int.Parse(Console.ReadLine());
             CountToN(aux);
             CountToN(aux/2);
+            CountTo10();
             float var = GetRandomNumber();
             Console.WriteLine(var);
             ShowPlayerStatus(aux);
         }
-
+        /// <summary>
+        /// Asks for value and counts to it
+        /// </summary>
+        /// <param name="n"></param>
         private static void CountToN(int n)
         {
             for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        /// <summary>
+        /// Counts to 10
+        /// </summary>
+        private static void CountTo10()
+        {
+            for(int i = 1; i <= 10; i++)
             {
                 Console.WriteLine(i);
             }
@@ -28,7 +46,10 @@ namespace MyFirstMethod
             return 4.123f;
         }
 
-        //Void Returning Test
+        /// <summary>
+        /// Void Returning Test
+        /// </summary>
+        /// <param name="livesLeft"></param>
         private static void ShowPlayerStatus(int livesLeft)
         {
             /*
