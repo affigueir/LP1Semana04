c3466c49eba2306938ff511affd9a38dfd2559d7
affigueir <affigueir@gmail.com>
2022-03-22 17:19:02 +0000

    Fibonacci Working

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um número: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fibonacci(num));
+        }
+
+        private static int Fibonacci(int n)
+        {   
+            if (n >= 2) return Fibonacci(n - 1) + Fibonacci(n - 2);
+            
+            return 1;
+        }
+
+        private static int Factorial(int n)
+        {
+            if (n > 1) return Factorial(n - 1) * n;
+            return 1;
+        }
+    }
+}
+    
+
+
