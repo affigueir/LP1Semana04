2d479e7574a6e656c345d5830266484378db8edc
affigueir <affigueir@gmail.com>
2022-03-22 14:35:38 +0000

    Method Testing

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -8,6 +8,9 @@ namespace MyFirstMethod
         {
             CountTo10();
             CountTo10();
+            float var = GetRandomNumber();
+            Console.WriteLine(var);
+            ShowPlayerStatus(1);
         }
 
         private static void CountTo10()
@@ -16,6 +19,23 @@ namespace MyFirstMethod
             {
                 Console.WriteLine(i);
             }
+        }
+        // Testing Return
+        private static float GetRandomNumber()
+        {
+            return 4.123f;
+        }
+
+        //Void Returning Test
+        private static void ShowPlayerStatus(int livesLeft)
+        {
+            /*
+            if (livesLeft == 0)
+                return;
+            Console.WriteLine("Player is alive and well!");
+            */
+            if (livesLeft > 0)
+                Console.WriteLine("Player is alive and well!");
         }
     }
 }
